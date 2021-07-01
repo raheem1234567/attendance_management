8b7cc66f5592052d00f22451a5e96ae370c5af18
raheem1234567 <73927626+raheem1234567@users.noreply.github.com>
2021-07-01 12:01:00 +0100

    Add files via upload

--- static/js/firebaseConfig.js
+++ static/js/firebaseConfig.js
@@ -1,14 +1,12 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyB9dUhtlxe6ThnunKTeaKrUZiWVLBlOlqY",
+  authDomain: "attendance-management-d9ef0.firebaseapp.com",
+  projectId: "attendance-management-d9ef0",
+  storageBucket: "attendance-management-d9ef0.appspot.com",
+  messagingSenderId: "675529162614",
+  appId: "1:675529162614:web:25c02d02ecfbcd0f704457"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
